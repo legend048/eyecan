e90f0e62c985ee8869170f8bf19d6c88e0018a0f
legend048 <pklegend048@gmail.com>
2024-09-20 19:13:05 +0530

    routing url changed

--- main/routing.py
+++ main/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/speech", consumers.MyWebSocketConsumer.as_asgi()),
+    re_path(r"ws/chat/", consumers.MyWebSocketConsumer.as_asgi()),
     re_path('ws/video', consumers.VideoFrameConsumer.as_asgi()),
 ]
